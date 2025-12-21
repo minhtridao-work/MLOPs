adddab5000b10e38d87289603ddf954779a713a8
MinhTris <minhtridao.work@gmail.com>
2025-12-21 17:26:32 +0700

    Final Tuning for Project MLOPs

--- train_model.py
+++ train_model.py
@@ -95,10 +95,9 @@ def main():
     
     # Step 3: Hyperparameter tuning for Logistic Regression
     lr_param_grid = {
-    'C': [0.01, 0.1, 1, 5],
-    'penalty': ['l2'],
-    'solver': ['lbfgs'],
-    'max_iter': [200, 500]
+        'C': [0.01, 0.1, 1, 10, 100],
+        'solver': ['liblinear', 'lbfgs'],
+        'max_iter': [50, 100, 150, 200]
     }
     
     lr_tuned = hyperparameter_tuning(
@@ -130,13 +129,10 @@ def main():
     
     # Step 5: Hyperparameter tuning for Random Forest
     rf_param_grid = {
-    'n_estimators': [100, 200],
-    'max_depth': [5, 10, None],
-    'min_samples_split': [2, 10],
-    'min_samples_leaf': [1, 5],
-    'max_features': ['sqrt']
+        'n_estimators': [50, 100, 150, 200],
+        'max_depth': [5, 10, 15, 20, None],
+        'min_samples_split': [2, 5, 10]
     }
-
     
     rf_tuned = hyperparameter_tuning(
         RandomForestClassifier(random_state=42),
@@ -167,15 +163,11 @@ def main():
     
     # Step 7: Hyperparameter tuning for XGBoost
     xgb_param_grid = {
-    'n_estimators': [100, 200],
-    'max_depth': [3, 5],
-    'learning_rate': [0.05, 0.1],
-    'subsample': [0.8],
-    'colsample_bytree': [0.8],
-    'reg_alpha': [0, 0.1],
-    'reg_lambda': [1, 2]
-    }   
-
+        'n_estimators': [50, 100, 200],
+        'max_depth': [3, 5, 9],
+        'learning_rate': [0.01, 0.05 , 0.1],
+        'subsample': [0.7, 0.8, 0.9]
+    }
     
     xgb_tuned = hyperparameter_tuning(
         XGBClassifier(random_state=42, eval_metric='logloss'),
